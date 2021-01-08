084830bb054f1aa233e133cbc3f594033cb739db
Nugra Saragih <23220008@mahasiswa.itb.ac.id>
2021-01-08 17:01:25 +0700

    update db

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rest-app',
     'username' => 'root',
-    'password' => 'bismillah',
+    'password' => 'p@assw0rd!',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)

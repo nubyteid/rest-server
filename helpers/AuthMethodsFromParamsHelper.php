f006fb1f8d642e89598f3fa154acc8ff7bc9d963
Nugra Saragih <23220008@mahasiswa.itb.ac.id>
2021-01-08 19:14:16 +0700

    update auth

--- helpers/AuthMethodsFromParamsHelper.php
+++ helpers/AuthMethodsFromParamsHelper.php
@@ -49,7 +49,7 @@ class AuthMethodsFromParamsHelper
     private function getAuthMethods(){
         $authMethodsArray = null;
         if(Yii::$app->params['useHttpBasicAuth']) $authMethodsArray[] = HttpBasicAuth::class;
-        if(Yii::$app->params['useHttpBearerAuth']) $authMethodsArray[] = HttpBearerAuth::class;
+       // if(Yii::$app->params['useHttpBearerAuth']) $authMethodsArray[] = HttpBearerAuth::class;
         if(Yii::$app->params['useQueryParamAuth']) $authMethodsArray[] = QueryParamAuth::class;
         if($authMethodsArray == null) throw new NotSupportedException('You must choose at least one auth methods, configure your app\config\params.php for more options.');
         return $authMethodsArray;

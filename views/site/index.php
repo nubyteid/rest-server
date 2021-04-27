acba1beeeee9a429ae72ae446bdfba9d1820b8e7
nubyteid <nubyteid@gmail.com>
2021-04-27 14:22:55 +0700

    update

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Aplikasi Brankas Buku</h1>
+        <h1>Aplikasi Monitoring Bantuan Sosial</h1>
 
         <p class="lead">Aplikasi Server-Rest</p>
 
